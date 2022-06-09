548f878851f0398c7c21f43beb1fc931accb3ddd
Sara Esu <saraesu@gmail.com>
2022-06-09 12:47:49 +0200

    style:add title hover

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -16,13 +16,14 @@ export function Hero() {
           <Col xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 2 }} lg={{ span: 12, order: 1 }} xl={{ span: 12, order: 1 }} xxl={{ span: 12, order: 1 }} >
             <div className='hero-txt'>
               <h1>
-                <strong>
+                <strong className='hero-title'>
                   {item.hero.title.title1} <br />
                   {item.hero.title.title2} <br />
                   {item.hero.title.title3} <br />
                   </strong></h1>
               <strong className='hero-sub'>{item.hero.subtitle} <br /></strong>
-              <div><label><strong>{item.hero.label}</strong></label>
+              <div className='hero-l-content'>
+                <label><strong>{item.hero.label}</strong></label>
               <div className='hero-input'><input className='hero-email' type="email" name="email" id="email" placeholder='Email' />
                 <ModaleLogin />
               </div></div>
