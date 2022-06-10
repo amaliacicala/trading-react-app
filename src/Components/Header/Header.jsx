9f803155c7b35600afb7b9c356932133192bf4af
Amalia <cicala.amalia@gmail.com>
2022-06-10 11:06:51 +0200

    feat: add transition to all buttons; edit email input width in hero section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,7 +9,7 @@ export function Header() {
       <header className='header-header'>
           <h2 className='header-logo'>Logo</h2>
           <div>
-              <a className="header-anchor" href="#">Feature</a>
+              <a className="header-anchor" href="#">Features</a>
               <a className="header-anchor" href="#">Pricing</a>
               <ModaleLogin />
               <ModaleSignUp />
