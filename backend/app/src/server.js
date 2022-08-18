870d1bc59f33a8fc274e0bf0c786cada25b7bf49
Marco Ripa <marcoripa@hotmail.it>
2022-08-18 13:52:12 +0200

    Install and config dotenv; Install mysql, sequelize

--- backend/app/src/server.js
+++ backend/app/src/server.js
@@ -1,7 +1,10 @@
 //IMPORTS
 //Import Express library
 const express = require('express')
+
 const cors = require('cors')
+require("dotenv").config();
+
 
 //Create an instance of express
 const app = express()
@@ -11,10 +14,12 @@ app.use(cors())
 //Parsing all the request of content-type app/json
 app.use(express.json())
 
+//ROUTES
 //Home route
 app.get("/", (req, res) => {
     res.json({msg: 'Everything ok'})
 })
 
-const PORT = 4000
-app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`))
+//Set port and listen
+const port = process.env.PORT
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
