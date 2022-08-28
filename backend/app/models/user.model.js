2c1df5c178274e636ca088ecca426f8a0dead8ad
Amalia Cicala <cicala.amalia@gmail.com>
2022-08-28 19:10:26 +0200

    feat: fix syntax error in user.model.js

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, Sequelize) => {
-  return sequelize.define("users", {
-    name: { type: Sequelize.STRING },
-    surname: { type: Sequelize.STRING },
-    email: { type: Sequelize.STRING },
-    password: {
-      type: Sequelize.STRING,
-    },
-    prefix: { type: Sequelize.NUMBER },
-    phone: { type: Sequelize.NUMBER },
-  });
+	return sequelize.define('users', {
+		name: { type: Sequelize.STRING },
+		surname: { type: Sequelize.STRING },
+		email: { type: Sequelize.STRING },
+		password: {
+			type: Sequelize.STRING,
+		},
+		prefix: { type: Sequelize.INTEGER },
+		phone: { type: Sequelize.INTEGER },
+	});
 };
