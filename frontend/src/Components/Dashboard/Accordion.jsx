41b07a9c00e1b9a36e29a4277d320d8a3ab26790
Amalia Cicala <cicala.amalia@gmail.com>
2022-08-31 18:36:27 +0200

    feat: edit style of accordions in the dashboard for all media queries

--- frontend/src/Components/Dashboard/Accordion.jsx
+++ frontend/src/Components/Dashboard/Accordion.jsx
@@ -1,28 +1,37 @@
 import { Collapse } from 'antd';
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
-import json from '../../locales/dashboard.json'
+import json from '../../locales/dashboard.json';
 import TradingViewWidget, { Themes } from 'react-tradingview-widget';
-import { ReactNode } from 'react';
 
 const { Panel } = Collapse;
 
-export default function Accordion(){
-    return(
-        json.map(item =>{
-            return(
-                <Collapse className='accordions' ghost={true} bordered={false} defaultActiveKey={['1']} expandIcon={(panelProps) => ReactNode}>
-                <Panel className='accordion-header' header={item.symbol} key={item.id}>
-                    <TradingViewWidget
-                        symbol={item.symbol}
-                        theme={Themes.DARK}
-                        locale='uk'
-                        width='autosize'
-                    />
-                </Panel>
-        
-            </Collapse>
-            )
-        })
-    )
+export default function Accordion() {
+	return json.map((item) => {
+		return (
+			<Collapse
+				className='accordions'
+				ghost={true}
+				bordered={false}
+				defaultActiveKey={['1']}
+				expandIcon={({ isActive }) =>
+					isActive ? (
+						<i className='fa fa-minus' />
+					) : (
+						<i className='fa fa-plus' />
+					)
+				}
+			>
+				<Panel className='accordion-header' header={item.symbol} key={item.id}>
+					<TradingViewWidget
+						symbol={item.symbol}
+						theme={Themes.DARK}
+						locale='uk'
+						// width='autosize'
+						autosize
+					/>
+				</Panel>
+			</Collapse>
+		);
+	});
 }
--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -1,8 +1,5 @@
 import './dashboard.css';
-import { useState } from 'react';
 import { useUserContext } from '../../services/Authentication';
-import TradingViewWidget, { Themes } from 'react-tradingview-widget';
-import json from '../../locales/dashboard.json';
 import 'font-awesome/css/font-awesome.min.css';
 import Accordion from './Accordion';
 
@@ -10,42 +7,13 @@ export function Dashboard() {
 	const { user } = useUserContext();
 	const name = user;
 
-	const [toggle, setToggle] = useState(true);
-
 	return (
 		<main className='dashboard-main'>
 			<h1 className='dashboard-welcome-message'>
 				Welcome back, <span className='user-name'>{name}</span> 👋🏻
 			</h1>
-			<Accordion/>
 
-			{json.map((item) => {
-				return (
-					<div className='accordions' key={item.id} >
-						<input
-							type='checkbox'
-							id={item.id}
-							className='accordion-input'
-							onClick={() => setToggle(!toggle)}
-						/>
-						{/* <div className='accordion-header'>
-							<div className='accordion-header-items'>
-								<label for={item.id}>
-									<h2 className='accordion-button'>
-										{toggle ? (
-											<i className='fa fa-plus'></i>
-										) : (
-											<i class='fa fa-minus'></i>
-										)}
-									</h2>
-								</label>
-								<h6 className='accordion-header-text'>{item.symbol}</h6>
-							</div>
-						</div>
-					 */}
-					</div>
-				);
-			})}
+			<Accordion />
 		</main>
 	);
 }
