89ebbb01080c58270b6cffbd71b9d4ac01a7d981
Amalia Cicala <cicala.amalia@gmail.com>
2022-09-08 18:40:35 +0200

    feat: fix all compiling errors

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,37 +1,62 @@
 import { Row, Col } from 'antd';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import text from '../../locales/pricing+hero/txt.json';
-import "../Pricing/pricing.css";
-import "./hero.css";
+import '../Pricing/pricing.css';
+import './hero.css';
 import { ModaleLogin } from './ModalHero';
-import img from '../../assets/hero-section.svg'
-
+import img from '../../assets/hero-section.svg';
 
 export function Hero() {
-
-  return (
-    <>{text.map((item) => {
-      return (
-        <Row key={item.id} className='hero' justify='center'>
-          <Col xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 24, order: 2 }} lg={{ span: 12, order: 1 }} xl={{ span: 12, order: 1 }} xxl={{ span: 12, order: 1 }} >
-            <div className='hero-txt'>
-              <h1>
-                <strong className='hero-title'>
-                  {item.hero.title.title1}
-                </strong></h1>
-              <p className='hero-sub'>{item.hero.subtitle}</p>
-              <div className='hero-l-content'>
-                <label><p>{item.hero.label}</p></label>
-                <div className='hero-input'><input className='hero-email' type="email" name="email" id="email" placeholder='Email' />
-                  <ModaleLogin />
-                </div></div>
-            </div>
-          </Col>
-          <Col xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 24, order: 1 }} lg={{ span: 12, order: 2 }} xl={{ span: 12, order: 2 }} xxl={{ span: 12, order: 2 }} >
-            <img className="hero-img" alt='' src={img} />
-          </Col>
-        </Row>
-      )
-    })} </>
-  )
+	return (
+		<>
+			{text.map((item) => {
+				return (
+					<Row key={item.id} className='hero' justify='center'>
+						<Col
+							xs={{ span: 24, order: 2 }}
+							sm={{ span: 24, order: 2 }}
+							md={{ span: 24, order: 2 }}
+							lg={{ span: 12, order: 1 }}
+							xl={{ span: 12, order: 1 }}
+							xxl={{ span: 12, order: 1 }}
+						>
+							<div className='hero-txt'>
+								<h1>
+									<strong className='hero-title'>
+										{item.hero.title.title1}
+									</strong>
+								</h1>
+								<p className='hero-sub'>{item.hero.subtitle}</p>
+								<div className='hero-l-content'>
+									<label>
+										<p>{item.hero.label}</p>
+									</label>
+									<div className='hero-input'>
+										<input
+											className='hero-email'
+											type='email'
+											name='email'
+											id='email'
+											placeholder='Email'
+										/>
+										<ModaleLogin />
+									</div>
+								</div>
+							</div>
+						</Col>
+						<Col
+							xs={{ span: 24, order: 1 }}
+							sm={{ span: 24, order: 1 }}
+							md={{ span: 24, order: 1 }}
+							lg={{ span: 12, order: 2 }}
+							xl={{ span: 12, order: 2 }}
+							xxl={{ span: 12, order: 2 }}
+						>
+							<img className='hero-img' alt='' src={img} />
+						</Col>
+					</Row>
+				);
+			})}{' '}
+		</>
+	);
 }
--- frontend/src/Components/Pricing/Pricing.jsx
+++ frontend/src/Components/Pricing/Pricing.jsx
@@ -1,5 +1,5 @@
-import 'antd/dist/antd.css';
-import "./pricing.css";
+import 'antd/dist/antd.min.css';
+import './pricing.css';
 import '../Hero/hero.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { Typography } from 'antd';
@@ -7,56 +7,104 @@ import { ModaleLogin } from './ModalPricing';
 import txt from '../../locales/pricing+hero/txt.json';
 const { Title } = Typography;
 
-
 export function Pricing() {
-    return (
-        <div> {txt.map((item) => {
-            return (
+	return (
+		<div>
+			{' '}
+			{txt.map((item) => {
+				return (
+					<section key={item.id} className='pricing' id='pricing'>
+						<div className='card-l'>
+							<Title>
+								{item.pricing.title.only}
+								<strong> {item.pricing.title.priceSin} </strong>
+								{item.pricing.title.month}
+							</Title>
+							<div className='card-l-content'>
+								<ul className='fa-ul'>
+									<li>
+										<span className='fa-li'>
+											<i className='fa fa-check'></i>
+										</span>
+										{item.pricing.l1}
+									</li>
+									<li>
+										<span className='fa-li'>
+											<i className='fa fa-check'></i>
+										</span>
+										{item.pricing.l2}
+									</li>
+									<li>
+										<span className='fa-li'>
+											<i className='fa fa-check'></i>
+										</span>
+										{item.pricing.l3}
+									</li>
+									<li>
+										<span className='fa-li'>
+											<i className='fa fa-check'></i>
+										</span>
+										{item.pricing.l4}
+									</li>
+									<li>
+										<span className='fa-li'>
+											<i className='fa fa-check'></i>
+										</span>
+										{item.pricing.l5}
+									</li>
+								</ul>
+								<ModaleLogin />
+							</div>
+						</div>
 
-                <section key={item.id} className='pricing' id='pricing'>
-
-                    <div className='card-l'>
-                        <Title>
-                            {item.pricing.title.only}
-                            <strong> {item.pricing.title.priceSin} </strong>
-                            {item.pricing.title.month}
-                        </Title>
-                        <div className='card-l-content'>
-                            <ul className="fa-ul">
-                                <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l1}</li>
-                                <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l2}</li>
-                                <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l3}</li>
-                                <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l4}</li>
-                                <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l5}</li>
-                            </ul>
-                            <ModaleLogin />
-                        </div>
-                    </div>
-
-
-                    <div className='card-r'>
-                        <div className='pricing-title'>
-                            <Title>
-                                {item.pricing.title.only}
-                                <strong> {item.pricing.title.priceDx} </strong>
-                                {item.pricing.title.year}
-                            </Title>
-                            <Title level={4} underline="true" style={{ marginTop: '1rem' }}>{item.pricing.subtitle}</Title>
-                        </div>
-                        <ul className="fa-ul">
-                            <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l1}</li>
-                            <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l2}</li>
-                            <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l3}</li>
-                            <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l4}</li>
-                            <li><span className="fa-li"><i className="fa fa-check"></i></span>{item.pricing.l5}</li>
-                        </ul>
-                        <ModaleLogin />
-                    </div>
-
-                </section>
-
-            )
-
-        })}</div>
-    )
+						<div className='card-r'>
+							<div className='pricing-title'>
+								<Title>
+									{item.pricing.title.only}
+									<strong> {item.pricing.title.priceDx} </strong>
+									{item.pricing.title.year}
+								</Title>
+								<Title level={4} underline='true' style={{ marginTop: '1rem' }}>
+									{item.pricing.subtitle}
+								</Title>
+							</div>
+							<ul className='fa-ul'>
+								<li>
+									<span className='fa-li'>
+										<i className='fa fa-check'></i>
+									</span>
+									{item.pricing.l1}
+								</li>
+								<li>
+									<span className='fa-li'>
+										<i className='fa fa-check'></i>
+									</span>
+									{item.pricing.l2}
+								</li>
+								<li>
+									<span className='fa-li'>
+										<i className='fa fa-check'></i>
+									</span>
+									{item.pricing.l3}
+								</li>
+								<li>
+									<span className='fa-li'>
+										<i className='fa fa-check'></i>
+									</span>
+									{item.pricing.l4}
+								</li>
+								<li>
+									<span className='fa-li'>
+										<i className='fa fa-check'></i>
+									</span>
+									{item.pricing.l5}
+								</li>
+							</ul>
+							<ModaleLogin />
+						</div>
+					</section>
+				);
+			})}
+		</div>
+	);
 }
